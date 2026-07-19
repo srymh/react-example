af028f8e42bc10af31091e5c5c499b9ef06d81ac
srymh <31023018+srymh@users.noreply.github.com>
2026-07-19 17:17:28 +0900

    feat(card): タイトルのスタイルを太字に変更

--- packages/tanstack/table-v9/src/components/card.tsx
+++ packages/tanstack/table-v9/src/components/card.tsx
@@ -10,7 +10,7 @@ export function Card({
   return (
     <div className="flex h-100 w-full flex-col flex-nowrap gap-2 rounded border-4 border-slate-500 bg-slate-300 p-2">
       <div className="flex h-max shrink-0 flex-col items-start justify-center">
-        {title && <h2>{title}</h2>}
+        {title && <h2 className="font-bold">{title}</h2>}
         {description && (
           <p className="line-clamp-3 max-h-16 overflow-hidden text-xs text-gray-600">
             {description}
